c7c42d3811a5c3c9af4b66dc09101e121c5d8a5e
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-03-22 18:34:27 +0000

    displaying on/offline users

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -25,29 +25,43 @@ wss.broadcast = function broadcast(data) {
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  const countObjOn = {
+    type: 'userCount',
+    content: 'User has joined.',
+    userCount: wss.clients.size,
+  }
+  wss.broadcast(JSON.stringify(countObjOn));
 
 
-ws.on('message', (message) => {
-  const msgObj = JSON.parse(message);
-  if (msgObj.type === 'postMessage') {
-  let newMsg = {
-    type: 'incomingMessage',
-    id: uuidv1(),
-    username: msgObj.username,
-    content: msgObj.content
-  };
-  console.log(JSON.stringify(newMsg))
-  wss.broadcast(JSON.stringify(newMsg));
-  } else if (msgObj.type === 'postNotification') {
-  let notfObj = {
-    type: 'incomingNotification',
-    id: uuidv1(),
-    content: msgObj.content
-  };
-  wss.broadcast(JSON.stringify(notfObj));
- }
-});
+  ws.on('message', (message) => {
+    const msgObj = JSON.parse(message);
+    if (msgObj.type === 'postMessage') {
+    let newMsg = {
+      type: 'incomingMessage',
+      id: uuidv1(),
+      username: msgObj.username,
+      content: msgObj.content
+    };
+    //console.log(JSON.stringify(newMsg))
+    wss.broadcast(JSON.stringify(newMsg));
+    } else if (msgObj.type === 'postNotification' || msgObj.type === 'userCount') {
+    let notfObj = {
+      type: 'incomingNotification',
+      id: uuidv1(),
+      content: msgObj.content
+    };
+    wss.broadcast(JSON.stringify(notfObj));
+   }
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    const countObjOff = {
+      type: 'userCount',
+      content: 'User has left.',
+      userCount: wss.clients.size,
+    }
+    wss.broadcast(JSON.stringify(countObjOff));
+  });
 });
